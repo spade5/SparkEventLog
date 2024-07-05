7d98fe39ac9e34096ada597e7d075d08681e50de
hao <spade5@spade5.name>
2024-07-05 14:27:56 +0800

    fix

--- src/components/StreamingLog/index.tsx
+++ src/components/StreamingLog/index.tsx
@@ -25,13 +25,9 @@ const Styles: { [key: string]: { [key: string]: any } } = {
   }
 }
 
-const getCaterories = (cores: number) => {
-  const tasks =
-    cores === 1
-      ? ['Tasks']
-      : Array(cores)
-          .fill('Tasks-E')
-          .map((str, i) => `${str}${i}`)
+const getCaterories = (core_ids: { [key: string]: boolean }) => {
+  const cores = Object.keys(core_ids).length
+  const tasks = cores === 1 ? ['Tasks'] : Object.keys(core_ids).map((i) => `Tasks-E${i}`)
   return [...tasks, 'Stages', 'Jobs', 'Batches']
 }
 
@@ -41,15 +37,10 @@ const StreamingLog = (props: { dataUrl?: string; data?: string; cores?: number; 
   const [delayData, setDelayData] = useState<number[]>()
   const [TaskData, setTaskData] = useState<{ [key: string]: number[] }>({})
   const [processingData, setProcessingData] = useState<number[]>()
-  const [deserializeData, setDeserializeData] = useState<number[]>()
+  // const [deserializeData, setDeserializeData] = useState<number[]>()
   const { title, desc } = props
 
-  const [cores, setCores] = useState(1)
-
-  const categories = useMemo(() => {
-    const cats = getCaterories(cores)
-    return cats
-  }, [cores])
+  const [categories, setCategories] = useState<string[]>([])
 
   const getData = useCallback(async () => {
     // const url = '/data/1734.json'
@@ -102,8 +93,8 @@ const StreamingLog = (props: { dataUrl?: string; data?: string; cores?: number; 
             start: json['Task Info']['Launch Time'],
             end: json['Task Info']['Finish Time'],
             executorId: json['Task Info']['Executor ID'],
-            stage: json['Stage ID'],
-            ds_time: json['Task Metrics']['Executor Deserialize Time']
+            stage: json['Stage ID']
+            // ds_time: json['Task Metrics']['Executor Deserialize Time']
           }
           Tasks[json['Task Info']['Task ID']] = item
 
@@ -124,16 +115,23 @@ const StreamingLog = (props: { dataUrl?: string; data?: string; cores?: number; 
     })
 
     const cores = Object.keys(executorIDs).length
-    setCores(cores)
+    // setCores(cores)
+    const cats = getCaterories(executorIDs)
+    setCategories(cats)
+    // console.log(cores)
 
     const generateData = (obj: any, name: string, index: number) => {
       Object.keys(obj).forEach((key: any) => {
         const { start = 0, end = 0, executorId } = obj[key]
-        const idx: number = +executorId || index
-
-        if (obj[key].ds_time) {
-          deserializeData[StageToJob[obj[key].stage]] += obj[key].ds_time
+        let idx = index
+        if (executorId) {
+          const i = cats.find((cat) => cat === `Tasks-E${executorId}`)
+          idx = (i || 0) as number
         }
+
+        // if (obj[key].ds_time) {
+        //   deserializeData[StageToJob[obj[key].stage]] += obj[key].ds_time
+        // }
 
         if (obj[key].batch) {
           delayData[key] = obj[key].end - obj[key].batch + BATCH_INTERVAL
@@ -160,7 +158,7 @@ const StreamingLog = (props: { dataUrl?: string; data?: string; cores?: number; 
     const data: GanttDataProps[] = []
     const delayData: number[] = Array(Object.keys(Jobs).length).fill(0)
     const processingData: number[] = Array(Object.keys(Jobs).length).fill(0)
-    const deserializeData: number[] = Array(Object.keys(Jobs).length).fill(0)
+    // const deserializeData: number[] = Array(Object.keys(Jobs).length).fill(0)
 
     generateData(Jobs, 'Job', cores + 1)
     generateData(Stages, 'Stage', cores)
@@ -175,7 +173,7 @@ const StreamingLog = (props: { dataUrl?: string; data?: string; cores?: number; 
     console.log(processingData)
 
     setProcessingData(processingData)
-    setDeserializeData(deserializeData)
+    // setDeserializeData(deserializeData)
   }, [props.dataUrl])
 
   useEffect(() => {
